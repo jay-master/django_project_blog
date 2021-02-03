ef1dad50ac3848c0c820b24d2b4ad28cdd3fcff6
jay-master <57156119+jay-master@users.noreply.github.com>
2021-02-03 19:06:52 +0100

    template update

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Lee',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2021'
+    },
+    {
+        'author': 'Kim',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2021'
+    }
+]
+
+
 def home (request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about (request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
